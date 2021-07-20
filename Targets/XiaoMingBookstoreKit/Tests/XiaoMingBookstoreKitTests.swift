e4a5292627cebec5905f8b5203891e2cd1e5c81b
Charles <1070726094@qq.com>
2021-07-20 15:10:46 +0800

    workspace init

---
+++ Targets/XiaoMingBookstoreKit/Tests/XiaoMingBookstoreKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class XiaoMingBookstoreKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("XiaoMingBookstoreKit", "XiaoMingBookstoreKit")
+    }
+}
